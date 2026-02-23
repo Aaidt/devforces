614f645f285bcb76157d5c21f6899630fe8fdccd
Aaidt <aadits2004@gmail.com>
2026-02-23 21:49:36 +0530

    created resume get ep

--- apps/workers/src/index.ts
+++ apps/workers/src/index.ts
@@ -1,14 +1,15 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { Webhook } from "svix"
+import { prismaClient } from "@repo/db/prismaClient"
 import { verifyToken } from "@clerk/backend"
 import userRouter from "./routes/user"
 import adminRouter from "./routes/admin"
 
-export interface Variables {
-  user_id: string
-}
+export type AppVariables = { user_id: string }
+export type AppBindings = { RESUME_BUCKET: R2Bucket }
 
-const app = new Hono<{ Variables: Variables }>()
+const app = new Hono<{ Variables: AppVariables }>()
 
 app.use('/*', cors({
   origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
@@ -18,6 +19,70 @@ app.use('/*', cors({
   maxAge: 600,
   exposeHeaders: ['X-Custom']
 }))
+
+app.post("/webhook/clerk", async (c) => {
+  const payload = await c.req.text();
+  const svixId = c.req.header('svix-id');
+  const svixTimestamp = c.req.header('svix-timestamp');
+  const svixSignature = c.req.header('svix-signature');
+
+  if (!svixId || !svixTimestamp || !svixSignature) {
+    return c.text('Missing headers', 400);
+  }
+
+  try {
+    const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET!);
+    const evt = wh.verify(payload, {
+      'svix-id': svixId,
+      'svix-timestamp': svixTimestamp,
+      'svix-signature': svixSignature,
+    }) as any;
+
+    if (evt.type === 'user.created') {
+      const {
+        id,
+        first_name,
+        last_name,
+        image_url,
+        email_addresses,
+        phone_numbers
+      } = evt.data;
+      
+      const email = email_addresses?.[0]?.email_address;
+      const email_verified = email_addresses?.[0]?.verification?.status === 'verified';
+      
+      await prismaClient.user.upsert({
+        where: { clerk_id: id },
+        update: {
+          first_name: first_name,
+          last_name: last_name,
+          profile_pic: image_url || null,
+          email,
+          phone: phone_numbers?.[0]?.phone_number || null,
+          email_verified
+        },
+        create: {
+          clerk_id: id,
+          email,
+          first_name,
+          last_name,
+          email_verified,
+          role: "user"
+        }      
+      });
+    }else if(evt.type === 'user.deleted'){
+      const { id } = evt.data;
+      await prismaClient.user.delete({ where: { clerk_id: id } })
+    }else if(evt.type === 'user.updated'){
+      //TODO: update the required fields 
+    }
+
+    return c.json({ success: true }, 200);
+  } catch (err) {
+    console.error('Webhook error:', err);
+    return c.text('Verification failed', 400);
+  }
+})
 
 app.use(async (c, next) => {
   const authHeader = c.req.header("Authorization");
@@ -31,7 +96,6 @@ app.use(async (c, next) => {
        // authorizedParties: ['yourdomain.com'] 
      });
     c.set('user_id', claims.sub);
-    // c.set('session_id', claims.ses);
     await next();
   } catch (e) {
     return c.json({ error: 'Invalid token' }, 400);
--- apps/workers/src/routes/user.ts
+++ apps/workers/src/routes/user.ts
@@ -1,13 +1,11 @@
 import { Hono } from "hono";
-import type { Context } from "hono";
 import { r2 } from "@/lib/r2"
-import { Webhook } from "svix"
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 import { prismaClient } from "@repo/db/prismaClient"
-import { Variables } from "../index";
+import { AppVariables, AppBindings } from '../index';
 
-const userRouter = new Hono<{ Variables: Variables }>();
+const userRouter = new Hono<{ Bindings: AppBindings, Variables: AppVariables }>();
 
 userRouter.post("/resume/upload_url", async (c) => {
     const { filename, content_type } = await c.req.json();
@@ -40,68 +38,29 @@ userRouter.post("/resume/confirm", async (c) => {
 
 })
 
-userRouter.post("/webhook/clerk", async (c) => {
-    const payload = await c.req.text();
-    const svixId = c.req.header('svix-id');
-    const svixTimestamp = c.req.header('svix-timestamp');
-    const svixSignature = c.req.header('svix-signature');
-  
-    if (!svixId || !svixTimestamp || !svixSignature) {
-      return c.text('Missing headers', 400);
+userRouter.get("/resume", async (c) => {
+    const user_id = c.get('user_id');
+
+    const user = await prismaClient.user.findUnique({
+        where: { clerk_id: user_id },
+        select: { resume_obj_key: true }
+    });
+    if(!user?.resume_obj_key){
+        return c.json({ error: "User has no resume" }, 404);
     }
-  
-    try {
-      const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET!);
-      const evt = wh.verify(payload, {
-        'svix-id': svixId,
-        'svix-timestamp': svixTimestamp,
-        'svix-signature': svixSignature,
-      }) as any;
-  
-      if (evt.type === 'user.created') {
-        const {
-          id,
-          first_name,
-          last_name,
-          image_url,
-          email_addresses,
-          phone_numbers
-        } = evt.data;
-        
-        const email = email_addresses?.[0]?.email_address;
-        const email_verified = email_addresses?.[0]?.verification?.status === 'verified';
-        
-        await prismaClient.user.upsert({
-          where: { clerk_id: id },
-          update: {
-            first_name: first_name,
-            last_name: last_name,
-            profile_pic: image_url || null,
-            email,
-            phone: phone_numbers?.[0]?.phone_number || null,
-            email_verified
-          },
-          create: {
-            clerk_id: id,
-            email,
-            first_name,
-            last_name,
-            email_verified,
-            role: "user"
-          }      
-        });
-      }else if(evt.type === 'user.deleted'){
-        const { id } = evt.data;
-        await prismaClient.user.delete({ where: { clerk_id: id } })
-      }else if(evt.type === 'user.updated'){
-        //TODO: update the required fields 
-      }
-  
-      return c.json({ success: true }, 200);
-    } catch (err) {
-      console.error('Webhook error:', err);
-      return c.text('Verification failed', 400);
-    }
+    const key = user.resume_obj_key;
+
+    const resume = await c.env.RESUME_BUCKET.get(key);
+    if(!resume) return c.json({ error: "File missing in R2Bucket." }, 404);
+
+    const filename = key.split("-")[1];
+
+    return new Response(resume.body, {
+        headers: {
+            "ContentType": resume.httpMetadata?.contentType || "application/pdf",
+            "Content-Disposition": `attachment, filename=${filename}`
+        }
+    })
 })
 
 export default userRouter;
